fc24aa1369056be3fd4c8f28bfdc24d200bbea6c
Zneo111 <tevin.rutere@student.moringaschool.com>
2025-04-03 10:19:01 +0300

    adds index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+const form = document.getElementById("create-task-form");
+const taskInput = document.getElementById("new-task-description");
+
+const taskList = document.getElementById("tasks");
+
+form.addEventListener("submit", function(event) {
+
+  event.preventDefault();
+
+  const taskText = taskInput.value;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+});
+
 });
